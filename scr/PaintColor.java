7065b319dc1e312a9723e2ccf5e7796a798a2870
therilenix <nursultanbekeev1@gmail.com>
2026-04-26 16:51:43 +0500

    Create PaintColor.java

---
+++ scr/PaintColor.java
@@ -0,0 +1,13 @@
+package com.example.artmap;
+
+public class PaintColor {
+    public String name;
+    public int r, g, b;
+
+    public PaintColor(String name, int r, int g, int b) {
+        this.name = name;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
