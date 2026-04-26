595336461e564e0346660efc0293428e4f8afd97
therilenix <nursultanbekeev1@gmail.com>
2026-04-26 16:52:50 +0500

    Create PixelData.java

---
+++ scr/PixelData.java
@@ -0,0 +1,11 @@
+package com.example.artmap;
+
+public class PixelData {
+    public PaintColor color;
+    public Shade shade;
+
+    public PixelData(PaintColor c, Shade s) {
+        this.color = c;
+        this.shade = s;
+    }
+}
